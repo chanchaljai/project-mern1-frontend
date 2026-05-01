1e8e2e8166f3889b8e0e35e58484c733e7596376
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-01 13:51:20 +0530

    form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Register = () => {
   const [form, setForm] = useState({
@@ -13,14 +14,15 @@ const Register = () => {
     console.log(form)
   }
   return (
-    <div>
-      <div>
-        <h1>Register</h1>
-        <form onSubmit={handleSubmit} >
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <button type="submit">Register</button>
+    <div className='flex justify-center items-center h-screen bg-gray-200'>
+      <div className='bg-white p-8 rounded shadow-md w-full max-w-md'>
+        {msg && <p className='text-blue-600 mb-4'>{msg}</p>}
+        <h1 className='text-3xl font-bold text-center text-blue-600 mb-4'>Register</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
+          <input className='border border-gray-500 rounded p-2' type="text" name="name" placeholder="Name" />
+          <input className='border border-gray-500 rounded p-2' type="email" name="email" placeholder="Email" />
+          <input className='border border-gray-500 rounded p-2' type="password" name="password" placeholder="Password" />
+          <button className='bg-blue-600 text-white p-2 rounded text-xl hover:bg-blue-800 hover:cursor-pointer' type="submit">Register</button>
         </form>
       </div>
     </div>
