704af398f08b430ac1adb972d954596974d8fdf0
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-01 13:28:30 +0530

    register form without css

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [form, setForm] = useState({
+    " name": "",
+    " email": "",
+    " password": "",
+  })
+  const [msg, setMsg] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(form)
+  }
   return (
-    <div>Register</div>
+    <div>
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit} >
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
