f17e315434b846f2aa24add9e46ee95eb2ef92fa
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-03 20:30:31 +0530

    admin App.jsx setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import ProductDetails from './pages/ProductDetails'
 import { Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AddProduct from './admin/AddProduct'
+import ProductList from './admin/ProductList'
+import EditProduct from './admin/EditProduct'
 
 const router = createBrowserRouter([
   {
@@ -17,6 +20,22 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetails />,
+  },
+  {
+    path: "/admin/add-product",
+    element: <AddProduct />,
+  },
+  {
+    path: "/admin/product-list",
+    element: <ProductList />,
+  },
+  {
+    path: "/admin/edit-product/:id",
+    element: <EditProduct />,
   },
 ]);
 
--- src/admin/AddProduct.jsx
+++ src/admin/AddProduct.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
+    const [form, setForm] = useState({
+        title: "",
+        description: "",
+        category: "",
+        price: "",
+        image: "",
+        stock: "",
+    })
   return (
     <div>AddProduct</div>
   )
